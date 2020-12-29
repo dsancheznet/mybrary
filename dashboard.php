ea527a3da1a491ed5f8b8bc099e65ac06211af97
D.Sánchez <4673828+dsancheznet@users.noreply.github.com>
2020-12-29 09:42:50 +0000

    Update: dashboard

--- dashboard.php
+++ dashboard.php
@@ -85,12 +85,16 @@ Y88b  d88P 888         d8888888888 888  T88b  Y88b  d88P 888    888 888   d88P d
 					<li class="uk-nav-header">Filters</li>
 					<li class="uk-parent"><a href="#"><span data-uk-icon="icon: thumbnails" class="uk-margin-small-right"></span>Tags</a>
 						<ul id="side-tags" class="uk-nav-sub">
-<?php
-							$tmpTags = $myDB->getTagList();
-							foreach ($tmpTags as $tmpArray) {
-								echo '<li><a title="'.$tmpArray['caption'].'" href="#">'.$tmpArray['caption'].'</a></li>';
-							}
-?>
+<!--
+.d8888b.  8888888 8888888b.  8888888888 88888888888     d8888  .d8888b.   .d8888b.
+d88P  Y88b  888   888  "Y88b 888            888        d88888 d88P  Y88b d88P  Y88b
+Y88b.       888   888    888 888            888       d88P888 888    888 Y88b.
+"Y888b.     888   888    888 8888888        888      d88P 888 888         "Y888b.
+	 "Y88b.   888   888    888 888            888     d88P  888 888  88888     "Y88b.
+		 "888   888   888    888 888            888    d88P   888 888    888       "888
+Y88b  d88P  888   888  .d88P 888            888   d8888888888 Y88b  d88P Y88b  d88P
+"Y8888P"  8888888 8888888P"  8888888888     888  d88P     888  "Y8888P88  "Y8888P"
+-->
 						</ul>
 					</li>
 					<li class="uk-parent">
